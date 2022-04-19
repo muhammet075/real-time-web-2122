a00d0f5f512dde9742e37deac38f32ce24bfa174
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2022-04-19 11:03:32 +0200

    port fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });
