a70624b12eb48dc59657e1c3982bc0f5d172b99a
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-19 15:31:16 +0200

    testing socket

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,17 @@ var socket = io();
 
 var form = document.getElementById("submitForm");
 var input = document.getElementById("inputField");
+var messages = document.getElementById("messages");
+
+console.log(messages);
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
-    input.value = "";
+    var item = document.createElement("li");
+    item.textContent = input.value;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
   }
 });
