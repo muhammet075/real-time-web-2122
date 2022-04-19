d81afcd2052ec27c34359da17673b2ce9c1bf5a2
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-19 15:37:53 +0200

    kinda works

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,15 @@ var form = document.getElementById("submitForm");
 var input = document.getElementById("inputField");
 var messages = document.getElementById("messages");
 
-console.log(messages);
-
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
+
     var item = document.createElement("li");
     item.textContent = input.value;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
+    input.value = "";
   }
 });
